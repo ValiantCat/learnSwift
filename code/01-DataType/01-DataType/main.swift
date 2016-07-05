1848b3dbd0826b282a60d6545c1470cd13874348
jiangyi <jiangyi01@lianjia.com>
2016-07-05 16:26:14 +0800

    update

---
+++ code/01-DataType/01-DataType/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  01-DataType
+//
+//  Created by nero on 16/1/16.
+//  Copyright © 2016年 nero. All rights reserved.
+//🤔🤔🤔🤔
+
+import Foundation
+//  寒哥    iOS 大概一年 丁丁租房，
+
+// 不是 playground  
+print("hello World")
+var name = "zhangsan"
+let name2 = "lis"
+var name3:String = "zhangsan"
+let name4:String = "lisi"
+let 😛 = "😛"
+
+
+let number = 1
+let number2 = 1.0
+let result = number2 / Double(number)
+
+let char:Character = "A"
+for i in name.characters {
+    print(i)
+}
+
+print(char)
+print(😛)
+print(name)
+print(name2)
